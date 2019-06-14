dae26e18c7f275b11487d212a3e41c82293a3e4c
Jose <jlhm2077@gmail.com>
2019-06-14 13:14:28 +0200

    Primeros cambios

--- src/com/ipartek/formacion/utilidades/Ahorcado.java
+++ src/com/ipartek/formacion/utilidades/Ahorcado.java
@@ -1,13 +1,13 @@
 package com.ipartek.formacion.utilidades;
 
+import java.util.Scanner;
+
 public class Ahorcado {
 	public static final int INTENTOS = 7;
 
-	private char [] palabra = {'v', 'i', 'e', 'r', 'n', 'e', 's'};
-	public char [] palabraBuscar = new char[palabra.length];
-	
-	
-	
+	private char[] palabra = { 'v', 'i', 'e', 'r', 'n', 'e', 's' };
+	public char[] palabraBuscar = new char[palabra.length];
+
 	public Ahorcado() {
 		super();
 		for (int i = 0; i < palabraBuscar.length; i++) {
@@ -15,57 +15,93 @@ public class Ahorcado {
 		}
 	}
 
+	private boolean comprobrarCaracter(char caracter) {
 
-
-	public boolean jugar(char caracter) {
-			
 		boolean encontrado = false;
-		
+
+		// boolean[] encontrados = new boolean [palabra.length];
+
 		for (int i = 0; i < palabra.length; i++) {
-			if ( palabra[i] == caracter) {
-				setPalabraBuscar (caracter, i);
+			if (palabra[i] == caracter) {
+				setPalabraBuscar(caracter, i);
 				encontrado = true;
-			}			
+				// break;
+			}
 		}
-		
+
 		return encontrado;
 	}
 
-	public String  mostrarResultado() {
+	private String mostrarResultado() {
 		String palabra = "";
-		
+
 		for (int i = 0; i < palabraBuscar.length; i++) {
 			palabra = " " + palabraBuscar[i] + " ";
 		}
-		
+
 		return palabra;
 	}
+
+	private boolean comprobar() {
+
+		boolean exacto = true;
+
+		for (int i = 0; i < palabraBuscar.length; i++) {
+			if (palabraBuscar[i] == '_') {
+				exacto = false;
+			}
+
+		}
+
+		return exacto;
+	}
 	
+	
+
+	public void jugar( ) {
+		int intentos = INTENTOS;
+		char caract = '_';
+		
+		boolean acierto = false;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		do {
+
+			System.out.println("Escribe un caracter: \n");
+
+			caract = sc.next().charAt(0) ;
+
+			if (comprobrarCaracter(caract)) {
+				System.out.println("Muy bien \n");
+
+			} else {
+				System.out.println("Muy mallllll \n");
+				intentos = intentos - 1;
+			}
+
+			System.out.println(mostrarResultado());
+
+			acierto = comprobar();
+
+			if (intentos == 0 && !acierto) {
+				System.out.println("SE ACABO, HAS PERDIDO");
+
+			}
+		} while (intentos > 0 && !acierto);
+		
+		sc.close();
+	}
+
+//*******************************************************************************
+
+	public void setPalabraBuscar(char caracterBuscar, int posicion) {
+
+		this.palabraBuscar[posicion] = caracterBuscar;
+	}
 
 	public char[] getPalabraBuscar() {
 		return palabraBuscar;
 	}
 
-	public boolean comprobar (  )	{
-		
-		boolean exacto = true;
-		
-		for (int i = 0; i < palabraBuscar.length; i++) {
-			if ( palabraBuscar[i] == '_' ) {
-				exacto = false;
-			}
-			
-		}
-		
-		return exacto;
-	}
-
-
-	public void setPalabraBuscar(char caracterBuscar, int posicion) {
-		
-		this.palabraBuscar[posicion] = caracterBuscar;
-	}
-	
-	
-	
 }
--- src/com/ipartek/formacion/utilidades/JugarAhorcado.java
+++ src/com/ipartek/formacion/utilidades/JugarAhorcado.java
@@ -6,42 +6,21 @@ public class JugarAhorcado {
 
 	public static void main(String[] args) {
 		// Crear un a partida
+		/*
 		int intentos = Ahorcado.INTENTOS;
 		char caract = '_';
 		boolean acierto = false;
+		*/
 		
 		Ahorcado a = new Ahorcado();
 		System.out.println("*********** El ahorcado *********** \n");
-		System.out.println("Tienes " + intentos + "\n\n");
+		System.out.println("Tienes " + Ahorcado.INTENTOS + "\n\n");
 		
-		Scanner sc = new Scanner(System.in);
 		
-		do {
-			
-			System.out.println("Escribe un caracter: \n");
-			
-			caract = sc.next().charAt(0);
+		
+		a.jugar( );
 
-			if (a.jugar(caract)) {
-				System.out.println("Muy bien \n");
-
-			} else {
-				System.out.println("Muy mallllll \n");
-				intentos = intentos - 1;
-			}
-
-			System.out.println(a.mostrarResultado());
-			
-			acierto = a.comprobar();
-			
-			if (intentos == 0 && !acierto)
-			{
-				System.out.println("SE ACABO, HAS PERDIDO");
-				
-			}
-		} while (intentos > 0 && !acierto);
-
-		sc.close();
+		
 	}
 
 }
