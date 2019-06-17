f3f47701ee1edf3fe43496baf6fe35e9fb75a241
Jose <jlhm2077@gmail.com>
2019-06-17 12:52:27 +0200

    Añadir constantes

--- src/com/ipartek/formacion/utilidades/examen/Ejercicio1.java
+++ src/com/ipartek/formacion/utilidades/examen/Ejercicio1.java
@@ -13,9 +13,16 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 
+	final static int PESO_MOSCA=52;
+	final static int PESO_PLUMA=57;
+	final static int PESO_LIGERO=65;
+	final static int PESO_MEDIANO=90;
+	// >90 PESADO
+	// Máximo de boxeadores
+	final static int BOXEADORES = 30;
+	
 	public static void main(String[] args) throws Exception {
-		// Máximo de boxeadores
-		final int BOXEADORES = 30;
+		
 		int contador = 0;
 		int peso = 0;
 		boolean salir = false;
@@ -27,18 +34,18 @@ public class Ejercicio1 {
 
 			try {
 
-				peso = Integer.parseInt(sc.next());
+				peso = Integer.parseInt(sc.nextLine());
 
 				if (peso < 0)
 					throw new Exception("No se puede introducir un valor negativo");
 
 				if (peso > 90) {
 					System.out.println("Su peso es superior a 90 kg.");
-				} else if (peso >= 65 && peso <= 90) {
+				} else if (peso >= PESO_LIGERO && peso <= PESO_MEDIANO) {
 					System.out.println("Su peso está comprendido entre 65 y 90 kg.");
-				} else if (peso >= 58 && peso <= 64) {
+				} else if (peso > PESO_PLUMA && peso < PESO_LIGERO) {
 					System.out.println("Su peso está comprendido entre 58 y 64 kg.");
-				} else if (peso >= 52 && peso <= 67) {
+				} else if (peso >= PESO_MOSCA && peso <= PESO_PLUMA) {
 					System.out.println("Su peso está comprendido entre 52 y 67 kg.");
 				} else {
 					System.out.println("Su peso es inferior a 52 kg.");
